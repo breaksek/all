abff7521a2a8683a272a713393ba920da4f8b18c
breaksek <98497506+breaksek@users.noreply.github.com>
2023-06-01 19:14:59 +0700

    update scrapinglink/scrap.py

--- scrapinglink/scrap.py
+++ scrapinglink/scrap.py
@@ -2,6 +2,6 @@ import requests, re
 
 url = input(" [!] Masukkan Target = ")
 html = requests.get(url).text
-links = re.findall('"(https?://.*?)"',html)
+links = re.findall('"(https?://.*?)"', html)
 for link in links:
   print(link)

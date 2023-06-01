8c9e64177f0b93171ed2779e0a230fce258a751c
breaksek <98497506+breaksek@users.noreply.github.com>
2023-06-01 18:57:25 +0700

    update scrapinglink/scrap.py

--- scrapinglink/scrap.py
+++ scrapinglink/scrap.py
@@ -1,9 +1,10 @@
 import requests, re, os, time
 
 os.system("pip install requests")
-  
+os.clear()
+
 target = input(" [!] Masukkan Target = ")
 html = requests.get(target).text
-akhir = re.findall('"https?://.*?)"', html)
-for link in akhir:
+links = re.findall('"https?://.*?)"', html)
+for link in links:
   print(" [!] Result = " + link)

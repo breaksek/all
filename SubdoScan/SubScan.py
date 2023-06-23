dfc289684b84379554883195fb00977c84c92809
breaksek <98497506+breaksek@users.noreply.github.com>
2023-06-23 17:46:13 +0700

    update SubdoScan/SubScan.py

--- SubdoScan/SubScan.py
+++ SubdoScan/SubScan.py
@@ -10,3 +10,8 @@ def domain_scanner(domain_name,sub_domnames):
       except requests.ConnectionError;
       pass
   if __nama__ == '__main__':
+    dom_name = input("Masukkan Target Domain : ")
+    with open('Result.txt','r') as file:
+      nama = file.read()
+      sub_dom = name.splitlines()
+    domain_scanner(dom_name,sub_dom)

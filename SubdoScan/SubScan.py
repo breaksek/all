29d6c6919795739d78a0c0a4f62e737f1d3ba90a
breaksek <98497506+breaksek@users.noreply.github.com>
2023-06-23 18:55:29 +0700

    update SubdoScan/SubScan.py

--- SubdoScan/SubScan.py
+++ SubdoScan/SubScan.py
@@ -1,6 +1,6 @@
 import requests, os
 
-def domain_scanner(domain_name,sub_domnames)
+def domain_scanner(domain_name,sub_domnames):
   print(' [•] Result : ')
   for subdomain in subdomnames:
     url = f"https://{subdomain}.{domain_name}"
